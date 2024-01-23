f489051c39acd2e3ea44bdc370a21cc6d130ee28
Jyrki De Neve <jyrki.deneve@gmail.com>
2024-01-23 14:55:19 +0200

    Fix issue with replaceEnd

--- src/Http/Middleware/Redirects.php
+++ src/Http/Middleware/Redirects.php
@@ -34,7 +34,7 @@ class Redirects
             'full' => $uri,
             'fullNoQuery' => Str::beforeLast($uri, '?'),
             'fullWithTrailingSlash' => Str::finish($uri, '/'),
-            'fullWithoutTrailingSlash' => Str::replaceEnd($uri, '/'),
+            'fullWithoutTrailingSlash' => Str::replaceEnd('/', '', $uri),
             'path' => $requestUri,
             'pathNoQuery' => Str::beforeLast($requestUri, '?'),
         ];
